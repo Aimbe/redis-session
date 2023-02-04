e8f204cade1f0ccd273e5329d5baff8d635ca800
qkr2435 <qkr2435@gmail.com>
2023-02-04 23:56:02 +0900

    autowired injection 문제 임시 해결

--- src/main/java/kr/co/jay/session/config/RedisConfiguration.java
+++ src/main/java/kr/co/jay/session/config/RedisConfiguration.java
@@ -1,46 +1,44 @@
-//package kr.co.jay.session.config;
-//
-//import io.lettuce.core.dynamic.annotation.Value;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.data.redis.connection.RedisConnectionFactory;
-//import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
-//import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
-//import org.springframework.data.redis.core.HashOperations;
-//import org.springframework.data.redis.core.RedisTemplate;
-//import org.springframework.data.redis.serializer.StringRedisSerializer;
-//
-///*springBoot 2.0 이상은 application.yml 에 레디스 설정만 해줘도 사용 가능함*/
-//
-//@Configuration
-//public class RedisConfiguration {
-//
-//
-//	//@Value("${spring.data.redis.host}")
-//	private String host = "";
-//
-//	//@Value("${spring.data.redis.port}")
-//	private int port = 6379;
-//
-//	private String password= "";
-//
-//	@Bean
-//	public RedisConnectionFactory redisConnectionFactory() {
-//		RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
-//		redisStandaloneConfiguration.setHostName(host);
-//		redisStandaloneConfiguration.setPort(port);
-//		redisStandaloneConfiguration.setPassword(password);
-//		return new LettuceConnectionFactory(redisStandaloneConfiguration);
-//	}
-//
-//	@Bean
-//	public RedisTemplate<String, Object> redisTemplate() {
-//		RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
-//		redisTemplate.setConnectionFactory(redisConnectionFactory());
-//		redisTemplate.setKeySerializer(new StringRedisSerializer());
-//		redisTemplate.setValueSerializer(new StringRedisSerializer());
-//		return redisTemplate;
-//	}
-//
-//}
+package kr.co.jay.session.config;
+
+import io.lettuce.core.dynamic.annotation.Value;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.HashOperations;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+/*springBoot 2.0 이상은 application.yml 에 레디스 설정만 해줘도 사용 가능함*/
+
+@Configuration
+public class RedisConfiguration {
+
+
+	//@Value("${spring.data.redis.host}")
+	private String host = "";
+
+	//@Value("${spring.data.redis.port}")
+	private int port = 6379;
+
+	private String password= "";
+
+	public RedisConnectionFactory redisConnectionFactory() {
+		RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+		redisStandaloneConfiguration.setHostName(host);
+		redisStandaloneConfiguration.setPort(port);
+		redisStandaloneConfiguration.setPassword(password);
+		return new LettuceConnectionFactory(redisStandaloneConfiguration);
+	}
+
+	public RedisTemplate<String, Object> redisTemplate() {
+		RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+		redisTemplate.setConnectionFactory(redisConnectionFactory());
+		redisTemplate.setKeySerializer(new StringRedisSerializer());
+		redisTemplate.setValueSerializer(new StringRedisSerializer());
+		return redisTemplate;
+	}
+
+}
